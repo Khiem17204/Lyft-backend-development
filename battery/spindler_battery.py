07d4e34bd9bf73bb9b3065b588e71a0bf7e73610
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-01-13 16:06:06 +0700

    add tires, tires_test, and modify spindler_batter

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -5,7 +5,7 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
         self.current_date = current_date
     def needs_service(self):
-        date_need_serviced = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        date_need_serviced = self.last_service_date.replace(year=self.last_service_date.year + 3)
         if self.current_date > date_need_serviced:
             return True
         else:
